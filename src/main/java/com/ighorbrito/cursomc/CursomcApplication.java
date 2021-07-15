eaf8b790d33526d864891c5bdb977bdb6da4e10e
IghorBr <ighorbruno20@gmail.com>
2021-07-15 12:25:09 -0300

    Criado Relacionamento entre Categoria e Produto - e seu repository

--- src/main/java/com/ighorbrito/cursomc/CursomcApplication.java
+++ src/main/java/com/ighorbrito/cursomc/CursomcApplication.java
@@ -1,13 +1,49 @@
 package com.ighorbrito.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.ighorbrito.cursomc.domain.Categoria;
+import com.ighorbrito.cursomc.domain.Produto;
+import com.ighorbrito.cursomc.repositories.CategoriaRepository;
+import com.ighorbrito.cursomc.repositories.ProdutoRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria ctg1 = new Categoria(null, "Informática");
+		Categoria ctg2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		ctg1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		ctg2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().add(ctg1);
+		p2.getCategorias().addAll(Arrays.asList(ctg1, ctg2));
+		p3.getCategorias().add(ctg1);
+		
+		categoriaRepository.saveAll(Arrays.asList(ctg1, ctg2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
