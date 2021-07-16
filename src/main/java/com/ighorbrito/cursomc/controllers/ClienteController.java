fe320e62c79c7294f8f11c51ccb07524b833b52e
IghorBr <ighorbruno20@gmail.com>
2021-07-16 14:15:45 -0300

    PUT, DELETE, POST e GET para clientes

--- src/main/java/com/ighorbrito/cursomc/controllers/ClienteController.java
+++ src/main/java/com/ighorbrito/cursomc/controllers/ClienteController.java
@@ -1,13 +1,24 @@
 package com.ighorbrito.cursomc.controllers;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ighorbrito.cursomc.domain.Cliente;
+import com.ighorbrito.cursomc.dto.ClienteDTO;
 import com.ighorbrito.cursomc.services.ClienteService;
 
 @RestController
@@ -22,6 +33,52 @@ public class ClienteController {
 	public ResponseEntity<Cliente> findCliente(@PathVariable Integer id) {
 		Cliente obj = clienteService.searchById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insertCliente(@RequestBody @Valid ClienteDTO objDto) {
+		Cliente objCliente = clienteService.fromDTO(objDto); 
+		objCliente = clienteService.insertCliente(objCliente);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(objCliente.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> updateCliente(@PathVariable Integer id, @RequestBody @Valid ClienteDTO objDto) {
+		Cliente objCliente = clienteService.fromDTO(objDto);
+		
+		objCliente.setId(id);
+		objCliente = clienteService.updateCliente(objCliente);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> deleteCliente(@PathVariable Integer id) {
+		clienteService.deleteCliente(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAllCliente() {
+		List<ClienteDTO> list = clienteService.searchAll()
+									.stream().map(c -> new ClienteDTO(c)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPageCliente(
+			@RequestParam(value="page", defaultValue = "0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue = "ASC") String direction) {
+		
+		Page<ClienteDTO> list = clienteService.searchPage(page, linesPerPage, orderBy, direction)
+								.map(c -> new ClienteDTO(c));
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
--- src/main/java/com/ighorbrito/cursomc/services/ClienteService.java
+++ src/main/java/com/ighorbrito/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.ighorbrito.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.ighorbrito.cursomc.domain.Cliente;
+import com.ighorbrito.cursomc.dto.ClienteDTO;
 import com.ighorbrito.cursomc.repositories.ClienteRepository;
+import com.ighorbrito.cursomc.services.exceptions.DataIntegratityException;
 import com.ighorbrito.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,5 +27,46 @@ public class ClienteService {
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto de ID "+ id + " não encontrado " +
 				"Tipo: " + Cliente.class.getName()));
+	}
+
+	public Cliente insertCliente(Cliente obj) {
+		obj.setId(null);
+		return clienteRepository.save(obj);
+	}
+
+	public Cliente updateCliente(Cliente obj) {
+		Cliente newCliente = searchById(obj.getId());
+		updateData(newCliente, obj);
+		
+		return clienteRepository.save(newCliente);
+	}
+
+	private void updateData(Cliente newCliente, Cliente obj) {
+		newCliente.setNome(obj.getNome());
+		newCliente.setEmail(obj.getEmail());
+		
+	}
+
+	public void deleteCliente(Integer id) {
+		searchById(id);
+		try {			
+			clienteRepository.deleteById(id);		
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegratityException("Não é possível excluir a cliente que possui produtos");
+		}
+	}
+
+	public List<Cliente> searchAll() {
+		return clienteRepository.findAll();
+	}
+	
+	public Page<Cliente> searchPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return clienteRepository.findAll(pageRequest);
+
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
 }
