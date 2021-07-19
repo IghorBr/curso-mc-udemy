b6c58a9d64a540b49cb23f31a1fb78c46cc8f49e
IghorBr <ighorbruno20@gmail.com>
2021-07-19 09:38:13 -0300

    Validacao customizada para emails repetidos na insercao de clientes

--- src/main/java/com/ighorbrito/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/ighorbrito/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.ighorbrito.cursomc.controllers.exceptions.FieldMessage;
+import com.ighorbrito.cursomc.domain.Cliente;
 import com.ighorbrito.cursomc.domain.enums.TipoCliente;
 import com.ighorbrito.cursomc.dto.ClienteNewDTO;
+import com.ighorbrito.cursomc.repositories.ClienteRepository;
 import com.ighorbrito.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -29,6 +37,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
 		}
 		
+		Cliente cliAux = clienteRepository.findByEmail(objDto.getEmail());
+		
+		if (cliAux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
 		
 		// inclua os testes aqui, inserindo erros na lista
 
