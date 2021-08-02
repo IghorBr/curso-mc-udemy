63f2fc8db2b4bf2901ecbf229ba4e3b318a61ae7
IghorBr <ighorbruno20@gmail.com>
2021-08-02 14:26:55 -0300

    Implementando autorizacao

--- src/main/java/com/ighorbrito/cursomc/config/SecurityConfig.java
+++ src/main/java/com/ighorbrito/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.ighorbrito.cursomc.security.JWTAuthenticationFilter;
+import com.ighorbrito.cursomc.security.JWTAuthorizationFilter;
 import com.ighorbrito.cursomc.security.JWTUtil;
 
 @Configuration
@@ -57,7 +58,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		
-		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));		
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
