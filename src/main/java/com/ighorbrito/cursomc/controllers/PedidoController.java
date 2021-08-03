3f7f571007b8bc930987de94607ab9c441ba81b8
IghorBr <ighorbruno20@gmail.com>
2021-08-03 08:32:07 -0300

    Restringindo acessos aos pedidos

--- src/main/java/com/ighorbrito/cursomc/controllers/PedidoController.java
+++ src/main/java/com/ighorbrito/cursomc/controllers/PedidoController.java
@@ -5,15 +5,18 @@ import java.net.URI;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ighorbrito.cursomc.domain.Pedido;
+import com.ighorbrito.cursomc.dto.CategoriaDTO;
 import com.ighorbrito.cursomc.services.PedidoService;
 
 @RestController
@@ -38,6 +41,17 @@ public class PedidoController {
 				.buildAndExpand(pedido.getId()).toUri();
 		
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<Page<Pedido>> findPageCategoria(
+			@RequestParam(value="page", defaultValue = "0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue = "instant") String orderBy, 
+			@RequestParam(value="direction", defaultValue = "DESC") String direction) {
+		
+		Page<Pedido> list = pedidoService.searchPage(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
